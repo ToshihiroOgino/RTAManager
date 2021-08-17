23440d178f0742b05f65c778ee0b34f6b822da69
ToshihiroOgino <tonigo0725@gmail.com>
2021-08-17 18:58:44 +0900

    TimeAttackScoreの型を作ってみた ジャムボードのリンクを作成

---
+++ src/system/TimeAttackScore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTAManager.src.system
+{
+	class TimeAttackScore
+	{
+		/// <summary>作成者</summary>
+		public string Publisher { get; set; }
+
+		/// <summary>スコア</summary>
+		public string Record { get; set; }
+
+		/// <summary>記録時間</summary>
+		public string Time { get; set; }
+
+		/// <summary>タグ</summary>
+		public List<string> Tag { get; set; }
+
+		/// <summary>コメント</summary>
+		public string Comment { get; set; }
+	}
+}
