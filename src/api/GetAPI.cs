2b6e4e9805d9c4dbe132f043129a91c59d43e62a
yuukiiwai <yuuki.20020217@gmail.com>
2021-08-18 13:05:34 +0900

    add  <public string getAPI()>

--- src/api/GetAPI.cs
+++ src/api/GetAPI.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace RTAManager
 {
     class GetAPI
     {
+        public string getAPI()
+        {
+            string url = "https://script.google.com/macros/s/AKfycbxE-LW8I1jAILXPXrC77dqN6a4Q1ThNb1u5_3SK_TR6sTwF2KvOMoNSG4QltXTl70XPJg/exec";
+            WebClient wc = new WebClient();
+
+            return url;
+        }
         
     }
 }
